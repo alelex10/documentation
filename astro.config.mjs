feb87e0ce0a3a2af7804f50573d366fd2b60b56e
alelex10 <elvizvida@gmail.com>
2025-07-30 02:37:00 -0300

    feat: add MermaidJS and Playwright integration to Astro documentation
    - Updated package.json to include astro-expressive-code, remark-mermaidjs, and playwright as dependencies.
    - Created new MDX documentation for visualizing Mermaid graphs in Astro, detailing installation steps and configuration.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,26 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
-
 import mdx from "@astrojs/mdx";
+import expressiveCode from "astro-expressive-code";
+import remarkMermaid from 'remark-mermaidjs'
 
 // https://astro.build/config
 export default defineConfig({
+
+  markdown: {
+    remarkPlugins: [remarkMermaid]
+  },
   build: {
-      viewTransitions: {
-          // Define animaciones de fallback
-          fallbackName: "default",
-          fallbackDuration: 300,
-      },
-	},
+    viewTransitions: {
+      // Define animaciones de fallback
+      fallbackName: "default",
+      fallbackDuration: 300,
+    },
+  },
 
   vite: {
-      plugins: [tailwindcss()],
-	},
+    plugins: [tailwindcss()],
+  },
 
-  integrations: [mdx()],
+  integrations: [expressiveCode(), mdx()],
 });
